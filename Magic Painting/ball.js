86cab0d9895055a1ec326393a91e71b37242af58
s1nghularity <v.p.singh@gmail.com>
2022-07-19 09:14:29 -0700

    magic painting final update

--- Magic Painting/ball.js
+++ Magic Painting/ball.js
@@ -1,6 +1,19 @@
-var length = data.length;
-
 // you work goes here
-// -----------------------
-//mandalorian steel
+var ball;
+ball = create();
+
+var x = 0;
+var y = 0;
+var counter = 0;
+var data = [];
+
+for(var j=0; j<81; j++){
+    y = j*7;
+    for(var i=0; i<53; i++){
+        x = i*7;
+        create(x,y,painting[counter]);
+        data.push({x:x,y:y,color:painting[counter]});
+        counter++;
+    }
+}    
 
