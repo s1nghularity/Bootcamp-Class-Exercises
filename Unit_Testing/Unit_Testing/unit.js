da251085d0a1fcc9bace15bbd5ee0c1540cc6cd2
s1nghularity <v.p.singh@gmail.com>
2022-08-04 06:34:30 -0700

    unit testing exercise WIP

--- Unit_Testing/Unit_Testing/unit.js
+++ Unit_Testing/Unit_Testing/unit.js
@@ -1,26 +1,48 @@
 var name = []
 
-function greet(name){
-    if (greet(name))    
-        return `Hello,${name}`;
-    
-    else if (name = null) {
-        return "Hello there!"
-    } 
+export function greet(name) {
+  let result;
 
-    else if (name.isUpper){
-        return `Hello, ${name}`
-    }
+  switch (name()) {
+    case 'Elizabeth':
+      result = `Hello,${name}`;
+      break;
+    case name.isUpper:
+      result = `Hello, ${name}`;
+      break;
+    case greet([a, b]):
+      result = `Hello, ${a}, ${b}`;
+    default:
+      result = "Hello there!";
+  }
+}
+
+
+
+
+
+
+// function greet(name){
+//     const normal = (greet([name]))    
+//         return `Hello,${name}`;
     
-    else if (greet([a,b])){
-        return `Hello, ${a}, ${b}`     
-    }
+//     else if (name = null) {
+//         return "Hello there!"
+//     } 
+
+//     else if (name.isUpper){
+//         return `Hello, ${name}`
+//     }
+    
+//     else if (greet([a,b])){
+//         return `Hello, ${a}, ${b}`     
+//     }
  
-    else if (greet([...name])){
-        return `Hello, ${[name]}`     
-    }
+//     else if (greet([...name])){
+//         return `Hello, ${[name]}`     
+//     }
 
-};
+// };
 
 
 let result = greet("Elizabeth");
@@ -31,3 +53,9 @@ console.log(result2);
 
 let result3 = greet(['Jose','Pep']);
 console.log(result3)
+
+
+
+
+
+
--- Unit_Testing/Unit_Testing/unit.test.js
+++ Unit_Testing/Unit_Testing/unit.test.js
@@ -1,7 +1,22 @@
-// var utils  = require('course-utilities');
-// var greeting = utils.load('./unit.js', 'name');
+const { exportAllDeclaration } = require('@babel/types');
+var utils  = require('course-utilities');
+const { it } = require('node:test');
+var greeting = utils.load('./unit.js', 'name');
 
-// describe('test greet', () => {
+describe('test greet()', () => {
+    test('does function work', function(){
+        expect(greet('Elizabeth')).toBeCalled();
+
+        
+    });
+    
+    test('shout response', () => {
+        expect(name).toMatch(name.isUpper);
+    });
+
+
+
+});
 
 //     test('Object properties present', () => {
 //         expect(mit).toHaveProperty('city');
